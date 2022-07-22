4864cad9cb2f396f8406ebb9a0a52dc8982b52fb
Thiago Freitas <thiagofreitas2909@gmail.com>
2022-07-22 16:16:25 -0300

    Pequenas adições

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+console.log("Hello World");
+
+let valor1 = 10;
+let valor2 = 20;
+let resultado = valor1 + valor2;
+
+if (resultado > 30) {
+  console.log("Resultado maior que 30");
+} else if (resultado < 30) {
+  console.log("Resultado menor que 30");
+}
+if (resultado === 30) {
+  console.log("Resultado igual a 30");
+}
